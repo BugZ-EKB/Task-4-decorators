5ab798f90137bd6d54123874d3b88600c78073a0
BugZ-EKB <e.shorohov87@yandex.ru>
2023-08-27 16:44:31 +0400

    Solution 2

--- tasks/task.py
+++ tasks/task.py
@@ -9,7 +9,6 @@ def decorator_apply(lambda_func):
     pass
 
 
-# @decorator_apply(lambda user_id: user_id + 1)
-# def return_user_id(num: int) ->int:
-#     return num
-# print(return_user_id(42))
+@decorator_apply(lambda user_id: user_id + 1)
+def return_user_id(num: int) ->int:
+    return num
